4e176e162389277343102a7b486e6a0625deac4a
masudcodex <forester.masud@gmail.com>
2023-02-12 20:59:55 +0600

    project initiated

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
